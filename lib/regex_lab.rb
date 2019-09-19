eee2313a23f1bc6b3858c18cb2b2488a81adfe0f
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-19 14:45:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   # text.scan(/\b\w*(un)\w*(ing)\b/)
-   text.scan(/\bs\U*/)
+   text.scan(/\b(un)\S*/)
   # (/\b(un)[a-z]*(ing)\b/)
   # \bs\UN*
 end
