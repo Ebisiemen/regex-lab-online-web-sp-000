87bab04d6083f3d83b6c15bb44d9fcefe55cc058
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-19 14:18:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   text.scan(/\b(un) )
+   text.scan(/\b(un)[a-z]*(ing)\b/)
    
   # /w - words \A	Start of string 
 # (...)	Capture everything enclosed
