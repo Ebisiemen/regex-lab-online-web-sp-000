0a9b7edb9536fc5c657e68186bcc3319f4c5eae8
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-19 15:13:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-phone.match()
+phone.match(\D)
 end
   # /w - words \A	Start of string 
 # (...)	Capture everything enclosed
