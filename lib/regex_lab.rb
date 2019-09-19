bb6f3fb313b9a73c06e326ad277b0c8ef0dca1d8
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-19 14:02:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,8 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
    text.scan(/ )
    
-  # /w - words \A	Start of string
+  # /w - words \A	Start of string 
+# (...)	Capture everything enclosed
 end
 
 def words_five_letters_long(text)
