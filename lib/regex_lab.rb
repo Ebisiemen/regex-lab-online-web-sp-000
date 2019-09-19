3d4dc165b04d8a41dcd19412ee69f7dd132cccc8
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-19 15:51:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,7 +22,7 @@ end
 def valid_phone_number?(phone)
 # phone.match(/\d{10}/) ? true : false
 phone.gsub!(/\D/, '')
-# phone.match(/\d{10}/) ? true : false
+ phone.match(/\d{10}/) ? true : false
 end
   # /w - words \A	Start of string 
 # (...)	Capture everything enclosed
