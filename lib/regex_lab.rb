054a54e41ce167b052173fc07f2b4d5059794326
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-19 14:41:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   text.scan(/\b(un)[a-z]*(ing)\b/)
+   text.scan(/\b\w*(un)\w*(ing)\b/)
+  # (/\b(un)[a-z]*(ing)\b/)
 end
 
 def words_five_letters_long(text)
